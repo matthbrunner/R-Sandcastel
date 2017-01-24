3a4242101ce08b9a701f0c17a7e025553b97bc18
Matthias Brunner <matthias.brunner@geocom.ch>
2017-01-24 22:33:08 +0100

    simple modif

--- 20161222_RechtsseitigerTest.R
+++ 20161222_RechtsseitigerTest.R
@@ -1,7 +1,7 @@
 # Problem: Ein Produzent von Keksen behauptet, dass seine Produkte
 # ein Höchstanteil an gesättigten Fettsäuren von 2 g pro Keks enthalten.
-# In einer Stichprobe von 35 Keksen wurde ein Mittelwert von 2:1 g
-# gemessen. Nehmen Sie eine Standardabweichung von 0:25 g an.
+# In einer Stichprobe von 35 Keksen wurde ein Mittelwert von 2.1 g
+# gemessen. Nehmen Sie eine Standardabweichung von 0.25 g an.
 # Kann die Behauptung bei einem Signifikanzniveau von 5% verworfen
 # werden?
 qnorm(0.95, mean = 2, sd = 0.25/sqrt(35))
--- 20161222_ZeiseitigerTest.R
+++ 20161222_ZeiseitigerTest.R
@@ -6,6 +6,12 @@
 # Durchschnittsgewicht nicht verändert hat, bei einem Signifikanzniveau
 # von 5% verwerfen?
 
+xbar <- 14.5
+mu0 <- 15.4
+alpha <- 0.05
+sigma <- 2.5
+n <- 35
+
 qnorm(0.975, mean = 15.4, sd = 2.5/sqrt(35))
 qnorm(0.025, mean = 15.4, sd = 2.5/sqrt(35))
 
